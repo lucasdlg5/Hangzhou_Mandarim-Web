35efc0a0416d98b7937f73a5a75edb3a440dd8f7
lucasdlg5 <lucasdomingosleao55@hotmail.com>
2019-02-04 16:23:44 -0200

    Commit

--- hangzhou_viagens-web/src/components/App.js
+++ hangzhou_viagens-web/src/components/App.js
@@ -1,30 +1,49 @@
-import React, { Component } from 'react';
-// import logo from '../logo.svg';
+import React, { Component, Redirect } from 'react';
+import logo from '../logo.svg';
 import '../App.css';
 import Home from './Home';
 
 class App extends Component {
+
+  state = {
+    redirect: false
+  }
+  setRedirect = () => {
+    this.setState({
+      redirect: true
+    })
+  }
+
+  renderRedirect = () => {
+    if (this.state.redirect) {
+      return <Home />
+    }
+  }
+
   render() {
     return (
-      // <div className="App">
-      //   <header className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     <p>
-      //      Site Hangzhou Mandarim em desenvolvimento.
-      //     </p>
-      //     <a
-      //       className="App-link"
-      //       href="https://github.com/lucasdlg5/Hangzhou_Mandarim-Web"
-      //       target="_blank"
-      //       rel="noopener noreferrer"
-      //     >
-      //       Hangzhou Mandarim Github
-      //     </a>
-      //   </header>
-      // </div>
 
-      <div>
-        <Home />
+      <div className="App">
+       {this.renderRedirect()}
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+           Site Hangzhou Mandarim em desenvolvimento.
+          </p>
+          <a
+            className="App-link"
+            href="https://github.com/lucasdlg5/Hangzhou_Mandarim-Web"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Hangzhou Mandarim Github
+          </a>
+          <a className="App-link" rel="noopener noreferrer" onClick={this.setRedirect}>  Clique aqui para acessar o Beta em Desenvolvimento </a>
+
+         
+      
+          {/* <button onClick={this.setRedirect}>Redirect</button> */}
+        </header>
       </div>
     );
   }
--- hangzhou_viagens-web/src/components/Home.js
+++ hangzhou_viagens-web/src/components/Home.js
@@ -4,29 +4,21 @@ class Home extends Component {
     render(){
         return(
 
-        <section className="mainContainer">
+        <section className="mainContainer" id="mainContainer">
 
             <section >
-                <div className="item porfolioLeft">
-                
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam lobortis risus orci, at hendrerit felis rutrum sed. Praesent at pharetra quam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vestibulum ultrices quam sed dignissim cursus. Mauris euismod, orci in faucibus pulvinar, metus risus pellentesque ex, sit amet rutrum metus magna vehicula purus. Duis imperdiet molestie condimentum. Sed tempor diam ut nulla dictum lacinia. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur a magna id molestie. Praesent sed ipsum nec nulla pellentesque venenatis. Curabitur elementum est metus, sed scelerisque purus lobortis sed. In augue urna, ultrices eget tincidunt in, fermentum quis est.
+                <div className="item porfolioLeft" id="portfolioLeft">
+                    <div>
 
-Aenean ac condimentum libero, quis euismod lorem. Etiam aliquam sapien non finibus maximus. Vivamus at risus vestibulum erat elementum suscipit a vel lorem. Duis egestas dui turpis, non pharetra enim tempus eget. Nam venenatis mollis turpis. Donec nec blandit ligula. Pellentesque dapibus, urna eu pretium bibendum, mi risus pretium nisl, congue malesuada ligula nunc at metus. Vivamus iaculis arcu vitae nisl malesuada luctus. Maecenas odio sem, dignissim in ultrices nec, finibus ac leo. Mauris pharetra, dolor sit amet viverra laoreet, lorem nulla tempor felis, at tincidunt libero eros nec lacus. Ut id tellus auctor, dapibus massa non, scelerisque quam. Curabitur cursus at purus eget rhoncus. Nulla lobortis felis aliquam iaculis tristique. Praesent at nulla eu eros eleifend cursus auctor eu urna.
-
-Nam ut magna lacus. Sed consectetur lorem eget sem viverra faucibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas tempus, orci sed porta mollis, eros lorem consectetur neque, non lacinia massa purus et ante. Maecenas luctus id nisi sit amet elementum. Mauris porttitor massa velit, non porttitor orci convallis in. Pellentesque tempus nulla ante, sit amet euismod orci ullamcorper in. Nulla sit amet nunc magna. Aliquam gravida id lorem et aliquam.
-
-Nam sollicitudin justo id imperdiet tristique. Donec facilisis ipsum in aliquam cursus. In sed erat magna. Ut dapibus ex et sagittis auctor. Sed auctor dolor sed ligula varius, quis lobortis diam vestibulum. Praesent blandit commodo lorem ac varius. Proin libero quam, accumsan sed volutpat et, tempus eu urna. Duis odio diam, ultricies ac dapibus sit amet, finibus at libero.
-
-Nullam neque erat, sagittis sed pulvinar ac, congue a nulla. Nunc blandit pulvinar urna sit amet convallis. Maecenas fermentum lacus id feugiat dignissim. Integer sollicitudin neque nec leo euismod, a sagittis ex mollis. Nunc vitae turpis sed felis auctor imperdiet at a dolor. Maecenas quis mi lectus. Suspendisse vel ipsum ut lacus sollicitudin condimentum.
-
+                    </div>
                 </div>
             </section>
 
-            <section className="column">
-                <div className="item menuHeader">TesteDoItem1</div>
-                <div className="item content">TesteDoItem2</div>
-                <div className="item skills">TesteDoItem3</div>
-                <div className="item rodape">TesteDoItem4</div>
+            <section className="column" id="column">
+                <div className="item menuHeader" id="menuHeader">TesteDoItem1</div>
+                <div className="item content" id="content">TesteDoItem2</div>
+                <div className="item skills" id="skills">TesteDoItem3</div>
+                <div className="item rodape" id="rodape">TesteDoItem4</div>
             </section>
             
         </section>
