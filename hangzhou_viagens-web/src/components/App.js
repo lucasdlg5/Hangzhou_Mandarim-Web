640d1c1ff25aee79331ecd9832caa637b8694e46
lucasdlg5 <lucasdomingosleao55@hotmail.com>
2019-02-04 20:43:54 -0200

    Alteracoes para Travis

--- hangzhou_viagens-web/src/components/App.js
+++ hangzhou_viagens-web/src/components/App.js
@@ -23,32 +23,32 @@ class App extends Component {
   render() {
     return (
 
-      // <div className="App">
-      //  {this.renderRedirect()}
-      //   <header className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     <p>
-      //      Site Hangzhou Mandarim em desenvolvimento.
-      //     </p>
-      //     <a
-      //       className="App-link"
-      //       href="https://github.com/lucasdlg5/Hangzhou_Mandarim-Web"
-      //       target="_blank"
-      //       rel="noopener noreferrer"
-      //     >
-      //       Hangzhou Mandarim Github
-      //     </a>
-      //     <a className="App-link" rel="noopener noreferrer" onClick={this.setRedirect}>  Clique aqui para acessar o Beta em Desenvolvimento </a>
+      <div className="App">
+       {this.renderRedirect()}
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+           Site Hangzhou Mandarim em desenvolvimento.
+          </p>
+          <a
+            className="App-link"
+            href="https://github.com/lucasdlg5/Hangzhou_Mandarim-Web"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Hangzhou Mandarim Github
+          </a>
+          <a className="App-link" rel="noopener noreferrer" onClick={this.setRedirect}>  Clique aqui para acessar o Beta em Desenvolvimento </a>
 
          
       
-      //     {/* <button onClick={this.setRedirect}>Redirect</button> */}
-      //   </header>
-      // </div>
-
-      <div>
-        <Home />
+         
+        </header>
       </div>
+
+      // <div>
+      //   <Home />
+      // </div>
     );
   }
 }
